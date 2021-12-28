8613330c695de6ef17063a682473bc272a3b71d3
vigneshSelvarajMT <88506177+vigneshSelvarajMT@users.noreply.github.com>
2021-12-28 19:56:02 +0530

    Completed the firebase android code

---
+++ android/src/main/kotlin/com/vignesh/pushnotification/FirebaseMessageService.kt
@@ -0,0 +1,41 @@
+package com.vignesh.pushnotification
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import io.flutter.Log
+
+class FirebaseMessageService : FirebaseMessagingService() {
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "From: ${remoteMessage.from}")
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+        }
+    }
+
+    /**
+     * Called if the FCM registration token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the
+     * FCM registration token is initially generated so this is where you would retrieve the token.
+     */
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+    }
+
+
+    companion object {
+        private const val TAG = "KwickMetrics/MessageService"
+    }
+}
